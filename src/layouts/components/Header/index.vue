ffe7ed3b6d891656ed717de33c5aab37357b65b4
Liu <1102549801@qq.com>
2024-04-16 01:17:01 +0800

    feat(BreadCrumb): 实现BreadCrumb 面包屑
    - 实现BreadCrumb 面包屑(基于静态路由)
    - 安装vueuse并使用防抖函数
    - 监听浏览器宽度实现是否显示面包屑

--- src/layouts/components/Header/index.vue
+++ src/layouts/components/Header/index.vue
@@ -1,5 +1,31 @@
 <script setup>
+import { ref, onBeforeUnmount, onMounted } from 'vue';
+import { useDebounceFn } from '@vueuse/core';
 import Collapse from './components/Collapse.vue';
+import BreadCrumb from './components/BreadCrumb.vue';
+
+/** 监听窗口大小变化，进行是否显示 */
+const showTool = ref(true); // 是否显示
+const screenWidth = ref(0); // 屏幕宽度
+
+// 设置屏幕宽度和是否显示工具的初始值
+const setInitialValues = () => {
+  screenWidth.value = document.body.clientWidth;
+  showTool.value = screenWidth.value >= 865;
+};
+
+const listeningWindow = useDebounceFn(() => {
+  screenWidth.value = document.body.clientWidth;
+  showTool.value = screenWidth.value >= 865;
+}, 100);
+// 添加事件监听器
+window.addEventListener('resize', listeningWindow, false);
+// 在组件挂载时设置初始值
+onMounted(setInitialValues);
+// 在组件卸载前移除事件监听器
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', listeningWindow);
+});
 </script>
 
 <template>
@@ -7,6 +33,8 @@ import Collapse from './components/Collapse.vue';
     <div class="header-left">
       <!-- 左侧菜单展开和折叠图标 -->
       <Collapse />
+      <!-- 面包屑 -->
+      <BreadCrumb v-if="showTool" />
     </div>
   </div>
 </template>
---
+++ src/utils/index.js
@@ -0,0 +1,44 @@
+/**
+ * @description 使用递归过滤出需要渲染在左侧菜单动态数据的列表 (需剔除 isHide == 0 隐藏的菜单)
+ * @param {Array} menuList 菜单列表
+ * @returns {Array}
+ * */
+export function getShowDynamicMenuList(menuList) {
+  let newMenuList = JSON.parse(JSON.stringify(menuList));
+  return newMenuList.filter((item) => {
+    return item.isHide === '1' || item.meta?.isHide === '1';
+  });
+}
+
+/**
+ * @description 使用递归过滤出需要渲染在左侧菜单静态的列表 (isHide == 0 隐藏的菜单无需剔除)，解决字典详情isHide == 0 隐藏菜单没法访问该页面问题，最后递归菜单组件再将 isHide == 0 的去除不进行显示。
+ * @param {Array} menuList 菜单列表
+ * @returns {Array}
+ * */
+export function getShowStaticMenuList(menuList) {
+  let newMenuList = JSON.parse(JSON.stringify(menuList));
+  return newMenuList.filter((item) => {
+    return (
+      item.isHide === '1' ||
+      item.meta?.isHide === '1' ||
+      item.isHide === '0' ||
+      item.meta?.isHide === '0'
+    );
+  });
+}
+
+/**
+ * @description 使用递归找出所有面包屑存储到 pinia 中
+ * @param {Array} menuList 菜单列表
+ * @param {Array} parent 父级菜单
+ * @param {Object} result 处理后的结果
+ * @returns {Object}
+ */
+export const getAllBreadcrumbList = (menuList, parent = [], result = {}) => {
+  for (const item of menuList) {
+    result[item.path] = [...parent, item];
+
+    if (item.children) getAllBreadcrumbList(item.children, result[item.path], result);
+  }
+  return result;
+};
