5f447067305de271886e1573cf884d820167a333
Liu <1102549801@qq.com>
2024-04-17 15:24:53 +0800

    feat(实现布局切换): 实现动态切换布局
    - 实现动态切换布局

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,17 +1,29 @@
 <script setup>
-// import LayoutVertical from './LayoutVertical/index.vue';
-// import LayoutClassic from './LayoutClassic/index.vue';
-import LayoutColumns from './LayoutColumns/index.vue';
-// import LayoutHorizontal from './LayoutHorizontal/index.vue';
 import ThemeConfig from '@/layouts/components/ThemeConfig/index.vue';
+import { computed } from 'vue';
+// 全局主题配置
+import useGlobalStore from '@/stores/modules/global.js';
+import LayoutVertical from '@/layouts/LayoutVertical/index.vue';
+import LayoutColumns from '@/layouts/LayoutColumns/index.vue';
+import LayoutClassic from '@/layouts/LayoutClassic/index.vue';
+import LayoutHorizontal from '@/layouts/LayoutHorizontal/index.vue';
+
+const LayoutComponent = {
+  vertical: LayoutVertical,
+  columns: LayoutColumns,
+  classic: LayoutClassic,
+  horizontal: LayoutHorizontal
+};
+
+// 全局主题仓库
+const globalStore = useGlobalStore();
+// 获取布局格式
+const layout = computed(() => globalStore.layout);
 </script>
 
 <template>
   <ThemeConfig />
-  <!--<LayoutVertical />-->
-  <!--<LayoutClassic />-->
-  <LayoutColumns />
-  <!--<LayoutHorizontal />-->
+  <component :is="LayoutComponent[layout]" />
 </template>
 
 <style scoped></style>
