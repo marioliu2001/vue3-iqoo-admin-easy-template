69984f4156790497ad7f370d0c0d4b4b743d6ef8
Liu <1102549801@qq.com>
2024-04-17 21:28:08 +0800

    feat(开屏动画): 开屏动画
    - 开屏动画

--- src/hooks/screen/index.js
+++ src/hooks/screen/index.js
@@ -1,5 +1,5 @@
-import { reactive } from "vue";
-import { useBreakpoints } from "@vueuse/core";
+import { reactive } from 'vue';
+import { useBreakpoints } from '@vueuse/core';
 
 export const breakpointsEnum = {
   xl: 1600,
@@ -12,11 +12,11 @@ export const breakpointsEnum = {
 export const useScreenStore = () => {
   const breakpoints = reactive(useBreakpoints(breakpointsEnum));
   // 手机端
-  const isMobile = breakpoints.smaller("sm");
+  const isMobile = breakpoints.smaller('sm');
   // pad端
-  const isPad = breakpoints.between("sm", "md");
+  const isPad = breakpoints.between('sm', 'md');
   // pc端
-  const isDesktop = breakpoints.greater("md");
+  const isDesktop = breakpoints.greater('md');
   return {
     breakpoints,
     isMobile,
